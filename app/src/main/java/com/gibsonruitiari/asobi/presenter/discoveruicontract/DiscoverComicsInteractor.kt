07999382ee1dfc5a63f39358759f7c58c8da3230
GibsonRuitiari <gibsonruitiari@gmail.com>
2022-07-03 00:34:53 +0300

    added view data models

--- app/src/main/java/com/gibsonruitiari/asobi/presenter/discoveruicontract/DiscoverComicsInteractor.kt
+++ app/src/main/java/com/gibsonruitiari/asobi/presenter/discoveruicontract/DiscoverComicsInteractor.kt
@@ -3,11 +3,16 @@ package com.gibsonruitiari.asobi.presenter.discoveruicontract
 import androidx.paging.PagingData
 import com.gibsonruitiari.asobi.common.Action
 import com.gibsonruitiari.asobi.common.Effect
-import com.gibsonruitiari.asobi.data.datamodels.SManga
+import com.gibsonruitiari.asobi.common.State
 import com.gibsonruitiari.asobi.presenter.uiModels.ViewComics
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 
+data class DiscoverComicsState(val comicsData:DiscoverComicsResult):State{
+    companion object{
+        val Empty = DiscoverComicsState(DiscoverComicsResult.EMPTY)
+    }
+}
 sealed class DiscoverComicsAction: Action {
     object LoadComics:DiscoverComicsAction()
     data class Error(val message:String=""):DiscoverComicsAction()
