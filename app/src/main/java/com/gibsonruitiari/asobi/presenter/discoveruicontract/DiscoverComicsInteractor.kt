95fef246c77cc9fb5553b46b7e081fd0b610b4b0
GibsonRuitiari <gibsonruitiari@gmail.com>
2022-07-03 00:34:02 +0300

    added view data models

--- app/src/main/java/com/gibsonruitiari/asobi/presenter/discoveruicontract/DiscoverComicsInteractor.kt
+++ app/src/main/java/com/gibsonruitiari/asobi/presenter/discoveruicontract/DiscoverComicsInteractor.kt
@@ -4,7 +4,9 @@ import androidx.paging.PagingData
 import com.gibsonruitiari.asobi.common.Action
 import com.gibsonruitiari.asobi.common.Effect
 import com.gibsonruitiari.asobi.data.datamodels.SManga
+import com.gibsonruitiari.asobi.presenter.uiModels.ViewComics
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 sealed class DiscoverComicsAction: Action {
     object LoadComics:DiscoverComicsAction()
@@ -14,9 +16,18 @@ sealed class DiscoverComicsAction: Action {
 sealed class DiscoverComicsSideEffect: Effect {
     data class Error(val message: String=""):DiscoverComicsSideEffect()
 }
-class DiscoverComicsResult{
+data class DiscoverComicsResult(val latestComics:DiscoverComicsData,
+val popularComics:DiscoverComicsData,val ongoingComics:DiscoverComicsData,
+val completedComics:DiscoverComicsData){
     companion object{
-
+        val EMPTY = DiscoverComicsResult(DiscoverComicsData.EMPTY,
+            DiscoverComicsData.EMPTY,DiscoverComicsData.EMPTY,DiscoverComicsData.EMPTY)
     }
-    data class DiscoverComicsData(val comicsData:Flow<PagingData<SManga>>)
+    data class DiscoverComicsData(val comicsData:Flow<PagingData<ViewComics>>,
+                                  val errorMessage:String?=null){
+        companion object{
+            val EMPTY = DiscoverComicsData(comicsData = emptyFlow(),
+            errorMessage = null)
+        }
+    }
 }
