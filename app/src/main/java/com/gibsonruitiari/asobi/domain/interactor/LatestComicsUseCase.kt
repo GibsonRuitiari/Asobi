c950b3f220920b818b9adc2947ae71ef92d2bfcd
GibsonRuitiari <gibsonruitiari@gmail.com>
2022-07-03 00:41:56 +0300

    added data sources: LatestComicsDataSource.kt, OngoingComicsDataSource.kt, PopularComicsDataSource.kt, CompletedComicsDataSource.kt

--- app/src/main/java/com/gibsonruitiari/asobi/domain/interactor/LatestComicsUseCase.kt
+++ app/src/main/java/com/gibsonruitiari/asobi/domain/interactor/LatestComicsUseCase.kt
@@ -1,12 +1,21 @@
 package com.gibsonruitiari.asobi.domain.interactor
 
+import com.gibsonruitiari.asobi.domain.pagingdatasource.CompletedComicsDataSource
 import com.gibsonruitiari.asobi.domain.pagingdatasource.LatestComicsDataSource
+import com.gibsonruitiari.asobi.domain.pagingdatasource.OngoingComicsDataSource
+import com.gibsonruitiari.asobi.domain.pagingdatasource.PopularComicsDataSource
 import com.gibsonruitiari.asobi.presenter.discoveruicontract.DiscoverComicsResult
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-class LatestComicsUseCase constructor(private val latestComicsDataSource: LatestComicsDataSource):FlowUseCase<Unit,
+class LatestComicsUseCase constructor(private val latestComicsDataSource: LatestComicsDataSource,
+private val popularComicsDataSource: PopularComicsDataSource,
+private val ongoingComicsDataSource: OngoingComicsDataSource,
+private val completedComicsDataSource: CompletedComicsDataSource):FlowUseCase<Unit,
 DiscoverComicsResult>(){
-    override fun run(params: Unit): Flow<DiscoverComicsResult> {
-        TODO("Not yet implemented")
+
+    override fun run(params: Unit): Flow<DiscoverComicsResult> = flow {
+        popularComicsDataSource
+     //   DiscoverComicsResult()
     }
 }
