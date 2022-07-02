023272a242b37a7b1385740c2da0f6d750cb5a3b
GibsonRuitiari <gibsonruitiari@gmail.com>
2022-07-03 00:30:33 +0300

    added view data models

--- app/src/main/java/com/gibsonruitiari/asobi/common/Mapper.kt
+++ app/src/main/java/com/gibsonruitiari/asobi/common/Mapper.kt
@@ -1,6 +1,14 @@
 package com.gibsonruitiari.asobi.common
 
+import com.gibsonruitiari.asobi.data.datamodels.SManga
+import com.gibsonruitiari.asobi.presenter.uiModels.ViewComics
+
 interface Mapper<in Input,out Output>{
     fun map(input: Input):Output
 }
 
+
+val sMangaToViewComicMapper = object :Mapper<SManga,ViewComics>{
+    override fun map(input: SManga): ViewComics = ViewComics(comicName = input.comicName, comicLink = input.comicLink,
+        comicThumbnail = input.comicThumbnailLink)
+}
