4ca009eff96a5954dd7efb7b4c8341050cd8cfe1
GibsonRuitiari <gibsonruitiari@gmail.com>
2022-07-02 15:31:09 +0300

    added asynchronous api call method in network resource

--- app/src/main/java/com/gibsonruitiari/asobi/data/network/NetworkResource.kt
+++ app/src/main/java/com/gibsonruitiari/asobi/data/network/NetworkResource.kt
@@ -1,7 +1,41 @@
 package com.gibsonruitiari.asobi.data.network
 
+import com.gibsonruitiari.asobi.common.logging.Logger
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okio.IOException
+import java.net.HttpRetryException
+import java.net.UnknownHostException
+
+sealed class NetworkState{
+    object EMPTY:NetworkState()
+    object LOADING:NetworkState()
+    object SUCCESS:NetworkState()
+    data class Error(val errorMessage:String?=null):NetworkState()
+}
 sealed class NetworkResource <out T>{
     data class Success<out T>(val data:T):NetworkResource<T>()
     object Loading:NetworkResource<Nothing>()
     data class Error(val errorMessage:String?, val throwable: Throwable):NetworkResource<Nothing>()
+}
+
+/**
+ * By default work on the IO Dispatcher when making an apiCall
+ */
+suspend fun<T> asynchronousApiCall(apiCall:suspend ()->T,
+                                   logger: Logger,
+                                   apiCallDispatcher:CoroutineDispatcher=Dispatchers.IO):NetworkResource<T>{
+    return withContext(apiCallDispatcher){
+        try {
+            NetworkResource.Success(apiCall.invoke())
+        }catch (throwable:Throwable){
+            logger.e("The following error $throwable occurred when invoking the following api call $apiCall")
+            when(throwable){
+                // io exception encapsulates all http errors eg UnknownHostException etc
+                is IOException-> NetworkResource.Error(throwable = throwable, errorMessage = throwable.message)
+                else-> NetworkResource.Error(null,throwable)
+            }
+        }
+    }
 }
