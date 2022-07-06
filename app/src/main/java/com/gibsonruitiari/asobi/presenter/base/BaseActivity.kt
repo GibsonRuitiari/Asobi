ce4417752f4b365adfbf01bec76f307b03660539
GibsonRuitiari <gibsonruitiari@gmail.com>
2022-07-07 00:40:37 +0300

    added themes for both api 19 and api 21+

---
+++ app/src/main/java/com/gibsonruitiari/asobi/presenter/base/BaseActivity.kt
@@ -0,0 +1,7 @@
+package com.gibsonruitiari.asobi.presenter.base
+
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity(@LayoutRes contentLayoutId:Int):AppCompatActivity() {
+}
---
+++ app/src/main/java/com/gibsonruitiari/asobi/presenter/base/BaseSwipeRecyclerViewFragment.kt
@@ -0,0 +1,8 @@
+package com.gibsonruitiari.asobi.presenter.base
+
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+
+/*to be used by fragments showing paged data*/
+abstract class BaseSwipeRecyclerViewFragment<T:Any,VH:RecyclerView.ViewHolder>:Fragment(){
+}
