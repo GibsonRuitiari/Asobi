9ed60573d85aebb687d1d3c7f93cfb824eeebeaf
GibsonRuitiari <gibsonruitiari@gmail.com>
2022-07-22 11:08:27 +0300

    added search comics viewmodel

--- app/src/main/java/com/gibsonruitiari/asobi/ui/comicssearch/ComicsSearchFragment.kt
+++ app/src/main/java/com/gibsonruitiari/asobi/ui/comicssearch/ComicsSearchFragment.kt
@@ -1,4 +1,9 @@
 package com.gibsonruitiari.asobi.ui.comicssearch
 
-class ComicsSearchFragment {
+
+import com.gibsonruitiari.asobi.ui.MainNavigationFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class ComicsSearchFragment:MainNavigationFragment() {
+    private val comicsSearchViewModel:ComicsSearchViewModel by viewModel()
 }
